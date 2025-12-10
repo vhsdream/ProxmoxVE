2a69537ba5ada8c02d87eeabe6227bedeececf75
vhsdream <punk.sand7393@fastmail.com>
2025-12-10 08:01:06 -0500

    [FIX] Palmr: Add Garage S3 storage and config

--- ct/palmr.sh
+++ ct/palmr.sh
@@ -32,6 +32,69 @@ function update_script() {
     systemctl stop palmr-frontend palmr-backend
     msg_ok "Stopped Services"
 
+    if [[ -d /var/lib/garage ]]; then
+      msg_info "Installing & configuring Garage"
+      download_file "https://garagehq.deuxfleurs.fr/_releases/v2.1.0/x86_64-unknown-linux-musl/garage" "/usr/bin" && chmod +x /usr/bin/garage
+      GARAGE_RPC_SECRET="$(openssl rand -hex 32)"
+      GARAGE_ADMIN_TOKEN="$(openssl rand -base64 32)"
+      GARAGE_METRICS_TOKEN="$(openssl rand -base64 32)"
+      cat <<EOF >/etc/garage.toml
+metadata_dir = "/var/lib/garage/meta"
+data_dir = "/var/lib/garage/data"
+db_engine = "sqlite"
+
+replication_factor = 1
+
+rpc_bind_addr = "[::]:3901"
+rpc_public_addr = "127.0.0.1:3901"
+rpc_secret = "${GARAGE_RPC_SECRET}"
+
+[s3_api]
+s3_region = "garage"
+api_bind_addr = "[::]:9379"
+root_domain = ".s3.garage.localhost"
+
+[s3_web]
+bind_addr = "[::]:3902"
+root_domain = ".web.garage.localhost"
+index = "index.html"
+
+[admin]
+api_bind_addr = "[::]:3903"
+admin_token = "${GARAGE_ADMIN_TOKEN}"
+metrics_token = "${GARAGE_METRICS_TOKEN}"
+EOF
+      cat <<EOF >/etc/systemd/system/garage.service
+[Unit]
+Description=Garage Data Store
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+Environment='RUST_LOG=garage=info' 'RUST_BACKTRACE=1'
+ExecStart=/usr/bin/garage server
+StateDirectory=garage
+DynamicUser=true
+ProtectHome=true
+NoNewPrivileges=true
+LimitNOFILE=42000
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+      systemctl enable -q --now garage.service
+      NODE_ID="$(garage status | awk '{print $1}' | head -n1)"
+      $STD garage layout assign -z dc1 -c 1G "$NODE_ID" &&
+        $STD garage layout apply --version 1
+      $STD garage bucket create palmr-bucket
+      garage key create palmr-key | sed -n '2,4p' ~/.garage-palmr-key
+      $STD garage bucket allow --read --write --owner palmr-bucket palmr-key
+      KEY_ID="$(head -n1 ~/.garage-palmr-key | awk '{print $3}')"
+      SECRET_KEY="$(tail -n1 ~/.garage-palmr-key | awk '{print $3}')"
+      msg_ok "Installed & configured Garage"
+    fi
+
     cp /opt/palmr/apps/server/.env /opt/palmr.env
     rm -rf /opt/palmr
     fetch_and_deploy_gh_release "Palmr" "kyantech/Palmr" "tarball" "latest" "/opt/palmr"
